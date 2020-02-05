893a573c6d2f362d507342a8a1b24cca8c72d9d2
jhonsnoww <oochanbg@gmail.com>
2020-02-05 15:48:29 +0630

    functional and class base component

--- src/components/employee.js
+++ src/components/employee.js
@@ -1,12 +1,33 @@
 import React from 'react'
 
-export default class Employee extends React.Component{
-    render() {
-        return ( 
+// export default class Employee extends React.Component{
+//     render() {
+//         return ( 
+//            <div>
+//                <h1>{this.props.name}</h1>
+//                <p>{this.props.email}</p>
+//            </div>
+//         );
+//     }
+// }
+
+
+export default function  employee(props){
+   
+        return (
            <div>
-               <h1>{this.props.name}</h1>
-               <p>{this.props.email}</p>
-           </div>
-        );
-    }
-}
+               <h1>{props.name}</h1>
+               <p>{props.email}</p>
+           </div>);
+        
+        }
+
+// const employee = (props)=>{
+//     return ( 
+//         <div>
+//             <h1>{props.name}</h1>
+//             <p>{props.email}</p>
+//         </div>
+//      );
+// }
+// export default employee;
